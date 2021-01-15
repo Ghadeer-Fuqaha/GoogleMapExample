c71ebe9e9e56508df703d0151ca96f2980f15395
Ghadeer Fuqaha <ghadeer.alfuqaha@gmail.com>
2021-01-15 22:22:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/googlemapexample/MapsActivity.java
@@ -0,0 +1,62 @@
+package com.example.googlemapexample;
+
+
+import androidx.fragment.app.FragmentActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+// Beprogrammer.org
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //google map object
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // Beprogrammer.org
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+
+        //latitude and longitude of pharmacy one
+        double latitude =  31.968631;
+        double longitude = 35.897806;
+        // Add a marker in pharmacy and move the camera
+        LatLng Amman = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(Amman).title("Marker in pharmacy one"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Amman));
+
+
+        // Beprogrammer.org
+    }
+
+}
